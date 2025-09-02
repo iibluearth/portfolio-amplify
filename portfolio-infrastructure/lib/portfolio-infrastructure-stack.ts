183ff29cedb313564a51f14b65bccfb08a77d9cc
iibluearth <kaylamann777@gmail.com>
2025-09-02 14:21:51 -0500

    Added CDK

---
+++ portfolio-infrastructure/lib/portfolio-infrastructure-stack.ts
@@ -0,0 +1,58 @@
+import * as cdk from 'aws-cdk-lib';
+import * as amplify from '@aws-cdk/aws-amplify-alpha';
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
+
+export class PortfolioInfrastructureStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Amplify Application 
+    const amplifyApp = new amplify.App(this, 'PortfolioApplication', {
+      appName: 'Portfolio',
+
+      // Connect to my Github repo
+      sourceCodeProvider: new amplify.GitHubSourceCodeProvider ({
+        owner: 'iibluearth',
+        repository: 'portfolio',
+        oauthToken: cdk.SecretValue.secretsManager('github0token')
+      }),
+      
+      // Build Specification 
+      buildSpec: codebuild.BuildSpec.fromObjectToYaml({
+        version: '1.0',
+        frontend: {
+          phases: {
+            preBuild: {
+              commands: [
+                'echo "starting this build"',
+                'cd portfolio',
+                'npm install'
+            ],
+            },
+            build: {
+              commands: [
+                'echo "building our nextjs app..."',
+                'npm run build-and-export',
+                'echo "build is complete"'
+              ],
+            },
+          },
+          artifacts: {
+            baseDirectory: 'portfolio/out',
+            files: ['**/*'],
+          },
+          cache: {
+            paths: [
+              'node/modules/**/*',
+              '.next/cache/**/*'
+            ]
+          }
+        }
+      })
+
+    })
+    const mainBranch = amplifyApp.addBranch('main', {
+      autoBuild: true
+    })
+  }
+}
